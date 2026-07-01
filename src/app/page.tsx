95d9278aec2c2ba7939769cb81bd76db276b4048
Rowel Timbal <practice_practice25@yahoo.com>
2026-07-01 13:49:54 -0400

    Update homepage heating to Welcome to Tindera

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-forest-green">Tindera</h1>
+      <h1 className="text-4xl font-bold text-forest-green">Welcome to Tindera</h1>
     </main>
   );
 }
